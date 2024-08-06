bdffd961b80e67b1246d94b6b40926711c81b713
Hoang Minh Tam <goddie9x@yahoo.com>
2024-08-06 17:16:39 +0700

    init gateway

---
+++ src/main/java/com/librarymanager/APIGateway/ApiGatewayApplication.java
@@ -0,0 +1,23 @@
+package com.librarymanager.APIGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import io.netty.resolver.DefaultAddressResolverGroup;
+import reactor.netty.http.client.HttpClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public HttpClient httpClient() {
+		return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+	}
+}
---
+++ src/test/java/com/librarymanager/APIGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librarymanager.APIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
